5f4ab4b60276fc206fcebbb8aa9b4d0a833c3924
baclayonjonrel <145176543+baclayonjonrel@users.noreply.github.com>
2024-07-08 17:44:17 +0800

    detail view enhancements
    detail view enhancements

--- NetFlix Clone/Controllers/General/DetailViewController.swift
+++ NetFlix Clone/Controllers/General/DetailViewController.swift
@@ -10,13 +10,64 @@ import WebKit
 
 class DetailViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let titleLbl: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 25)
+        label.text = "Title"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let infoLbl: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = .systemFont(ofSize: 8)
+        label.text = "information"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let descriptionLbl: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 3
+        label.font = .systemFont(ofSize: 15)
+        label.text = "description"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let seeMoreButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("See More", for: .normal)
+        button.addTarget(self, action: #selector(didTapSeeMore), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isHidden = true
+        return button
+    }()
+    
     private let playButton: UIButton = {
         let button = UIButton()
         button.setTitle("Play", for: .normal)
         button.layer.borderColor = UIColor.systemBackground.cgColor
         button.layer.borderWidth = 1
         button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 5
+        button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -27,7 +78,7 @@ class DetailViewController: UIViewController {
         button.layer.borderColor = UIColor.systemBackground.cgColor
         button.layer.borderWidth = 1
         button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 5
+        button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -39,18 +90,34 @@ class DetailViewController: UIViewController {
         return imageView
     }()
     
+    private var isExpanded: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(heroImageView)
-        view.addSubview(playButton)
-        view.addSubview(downloadButton)
+        heroImageView.addBlackGradientLayerInForeground(frame: self.view.bounds, colors:[.clear, .black])
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(heroImageView)
+        contentView.addSubview(titleLbl)
+        contentView.addSubview(infoLbl)
+        contentView.addSubview(descriptionLbl)
+        contentView.addSubview(seeMoreButton)
+        contentView.addSubview(playButton)
+        contentView.addSubview(downloadButton)
         view.backgroundColor = .systemBackground
         activateConstraints()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        checkDescriptionHeight()
+    }
+    
+    @objc private func didTapSeeMore() {
+        isExpanded.toggle()
+        descriptionLbl.numberOfLines = isExpanded ? 0 : 3
+        seeMoreButton.setTitle(isExpanded ? "See Less" : "See More", for: .normal)
     }
     
     public func configure(with model: Media) {
@@ -61,33 +128,98 @@ class DetailViewController: UIViewController {
             return
         }
         heroImageView.sd_setImage(with: url, placeholderImage: placeholderImage)
+        
+        titleLbl.text = model.original_title ?? model.original_name ?? model.name
+        infoLbl.text = "\(model.release_date ?? "") | \(model.vote_count) votes | \(model.vote_average) average votes"
+        descriptionLbl.text = model.overview
+        
+        checkDescriptionHeight()
+    }
+    
+    private func checkDescriptionHeight() {
+        let text = descriptionLbl.text ?? ""
+        let width = descriptionLbl.frame.width
+        let font = descriptionLbl.font!
+        
+        if isTextExceedingLines(text: text, font: font, width: width, maxLines: 3) {
+            seeMoreButton.isHidden = false
+        } else {
+            seeMoreButton.isHidden = true
+        }
+    }
+    
+    private func isTextExceedingLines(text: String, font: UIFont, width: CGFloat, maxLines: Int) -> Bool {
+        let maxLinesHeight = CGFloat(maxLines) * font.lineHeight
+        let textHeight = text.boundingRect(
+            with: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude),
+            options: .usesLineFragmentOrigin,
+            attributes: [.font: font],
+            context: nil
+        ).height
+        return textHeight > maxLinesHeight
     }
     
     private func activateConstraints() {
-        let heroImageViewConstraints = [
-            heroImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            heroImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            heroImageView.topAnchor.constraint(equalTo: view.topAnchor),
-            heroImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.6)
-        ]
-        
-        let playButtonConstraints = [
-            playButton.topAnchor.constraint(equalTo: heroImageView.bottomAnchor, constant:30),
-            playButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            playButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            playButton.heightAnchor.constraint(equalToConstant: 40)
-        ]
-        
-        let downloadButtonConstraints = [
-            downloadButton.topAnchor.constraint(equalTo: playButton.bottomAnchor, constant: 10),
-            downloadButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            downloadButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            downloadButton.heightAnchor.constraint(equalToConstant: 40)
-        ]
-        
-        NSLayoutConstraint.activate(heroImageViewConstraints)
-        NSLayoutConstraint.activate(playButtonConstraints)
-        NSLayoutConstraint.activate(downloadButtonConstraints)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: -20),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            heroImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            heroImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            heroImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            heroImageView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 0.50),
+            
+            titleLbl.topAnchor.constraint(equalTo: heroImageView.bottomAnchor, constant: -10),
+            titleLbl.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLbl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            infoLbl.topAnchor.constraint(equalTo: titleLbl.bottomAnchor, constant: 5),
+            infoLbl.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            infoLbl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            playButton.topAnchor.constraint(equalTo: infoLbl.bottomAnchor, constant: 10),
+            playButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            playButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor, constant: -100),
+            playButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            downloadButton.topAnchor.constraint(equalTo: infoLbl.bottomAnchor, constant: 10),
+            downloadButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor, constant: 100),
+            downloadButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            downloadButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            descriptionLbl.topAnchor.constraint(equalTo: downloadButton.bottomAnchor, constant: 10),
+            descriptionLbl.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            descriptionLbl.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            seeMoreButton.topAnchor.constraint(equalTo: descriptionLbl.bottomAnchor, constant: 5),
+            seeMoreButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            seeMoreButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+        ])
     }
+}
 
+extension UIView {
+    // For insert layer in Foreground
+    func addBlackGradientLayerInForeground(frame: CGRect, colors: [UIColor]) {
+        let gradient = CAGradientLayer()
+        gradient.frame = frame
+        gradient.colors = colors.map { $0.cgColor }
+        self.layer.addSublayer(gradient)
+    }
+    
+    // For insert layer in background
+    func addBlackGradientLayerInBackground(frame: CGRect, colors: [UIColor]) {
+        let gradient = CAGradientLayer()
+        gradient.frame = frame
+        gradient.colors = colors.map { $0.cgColor }
+        self.layer.insertSublayer(gradient, at: 0)
+    }
 }
